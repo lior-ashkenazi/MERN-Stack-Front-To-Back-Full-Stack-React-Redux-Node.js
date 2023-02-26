191cf57fa2976e947b79aba6ee26caed90797a23
Lior Ashkenazi <lior.ashkenazi>
2023-02-26 19:37:02 +0200

    Completed Section 2

---
+++ DevConnector/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const usersRouter = require("./routes/api/users");
+const authRouter = require("./routes/api/auth");
+const profileRouter = require("./routes/api/profile");
+const postsRouter = require("./routes/api/posts");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+// Define Routes
+app.use("/api/users", usersRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/profile", profileRouter);
+app.use("/api/posts", postsRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
