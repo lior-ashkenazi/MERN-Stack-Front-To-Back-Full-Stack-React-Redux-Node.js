97923a972a5468aa00642878ef33ef1aee96c16e
Lior Ashkenazi <lior.ashkenazi>
2023-02-27 14:30:17 +0200

    Completed Section 3

--- DevConnector/server.js
+++ DevConnector/server.js
@@ -11,6 +11,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => {
   res.send("API Running");
 });
