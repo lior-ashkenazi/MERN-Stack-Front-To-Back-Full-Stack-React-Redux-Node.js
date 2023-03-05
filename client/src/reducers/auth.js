4ca72828caa0c492152f3afd25016ad3be122ca2
Lior Ashkenazi <lior.ashkenazi>
2023-03-05 17:32:33 +0200

    All commited files from the previous commit should be filled with the correct and (unforetunately) uncommited code that should have been commited beside the commited files themselves; some spoilers from future lectures added tothe code as well

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,35 @@
+function authReducer(state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload,
+      };
+    case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false,
+      };
+    case ACCOUNT_DELETED:
+    case AUTH_ERROR:
+    case LOGOUT:
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+      };
+    default:
+      return state;
+  }
+}
+
+export default authReducer;
