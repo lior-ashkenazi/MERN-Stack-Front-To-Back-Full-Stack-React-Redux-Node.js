b96195c6bb26db6b116a9a9628efe2a1f80c886e
Lior Ashkenazi <lior.ashkenazi>
2023-03-05 12:40:43 +0200

    Refactored client before Section 8

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,25 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Alert from "./components/layout/Alert";
+// Redux
+import {Provider} from "react-redux";
+import store from "./store";
+
 import "./App.css";
 
 const App = () => (
-    <BrowserRouter>
-        <Navbar/>
-        <Routes>
-            <Route path="/" element={<Landing/>}/>
-            <Route path="register" element={<Register/>}/>
-            <Route path="login" element={<Login/>}/>
-        </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <Navbar/>
+            <Alert/>
+            <Routes>
+                <Route path="/" element={<Landing/>}/>
+                <Route path="register" element={<Register/>}/>
+                <Route path="login" element={<Login/>}/>
+            </Routes>
+        </BrowserRouter>
+    </Provider>
 );
 
 export default App;
