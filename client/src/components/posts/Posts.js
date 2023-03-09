21f61313f1b5f604e69cc40e0eecd72d046a6e9e
Lior Ashkenazi <lior.ashkenazi>
2023-03-09 14:49:56 +0200

    Completed Section 11

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import Spinner from "../layout/Spinner";
-import PostItem from "../PostItem";
+import PostItem from "./PostItem";
+import PostForm from "./PostForm";
 import { getPosts } from "../../actions/post";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
